5e81c2cd134c38cda04a674060c1d02a747f003d
Pierre-Yves Lescoublet <pylscblt@gmail.com>
2024-03-05 21:18:04 +0100

    Issue #20 - Update title extraction in rss.xml - New parsing in TOP14 page to extract the image link

--- www/rss_blog.php
+++ www/rss_blog.php
@@ -19,8 +19,9 @@ file_put_contents( "/home/topsevenyu/www/" . c_file_blog, file_get_contents( c_u
 $path="/home/topsevenyu/www/img/";
 $login_img="/home/topsevenyu/www/top7_login.jpeg";
 
+# save picture
 #$url="http://www.lesbrevesdovalie.com";
-$url="http://www.lesbrevesdovalie.com/archives/les_breves_d_ovalie___";
+#$url="http://www.lesbrevesdovalie.com/archives/les_breves_d_ovalie___";
 $url="http://www.lesbrevesdovalie.com/tag/TOP%2014";
 $page1=getHtmlTags($url);
 if($page1) {
@@ -28,28 +29,28 @@ if($page1) {
 	#print_r($page1);
 	#echo "</pre>";
 	foreach($page1 as $match) {
-		if(strstr($match[3],"Les brèves d'Ovalie - Edition")) {
-			$url=$match[2];
-			#echo "$url";
-			// get 1st picture on last Edition
-			$page2=getHtmlTags($url);
-			if($page2) {
-				foreach($page2 as $match) {
-					if(strstr($match[2],"storage.canalblog.com")) {
-						#echo "<pre>";print_r($match);echo "</pre>";
-						$file=basename($match[2], ".jpg");
-						$filename=$file.".jpg";
-						#echo $path.$filename;
-						if(!file_exists($path.$filename)) {
-							file_put_contents($path.$filename, file_get_contents($match[2]));
-							if(filesize($path.$filename)>1000) copy($path.$filename, $login_img);
-						}
-						break;
-					}
-				}
-			}
-			break;
-		}
-	}
+        $pos = stripos($match[0], "data-pin-media");
+        if($pos !== false) {
+            $lines = explode("\n", $match[0]);
+            foreach($lines as $line) {
+                $pos = stripos($line, "data-pin-media");
+                if($pos !== false) {
+                    #echo "<pre>";
+                    #echo $line;
+                    #echo "</pre>";
+                    preg_match("/data-pin-media=(.*)/", $line, $match);
+                    $url = substr($match[1], 1, -1);
+                    #echo "<pre>$url</pre>";
+                    $name = end(explode("%", $url));
+                    #echo "<pre>$name</pre>";
+		            if(!file_exists($path.$name)) {
+			            file_put_contents($path.$name, file_get_contents($url));
+			            if(filesize($path.$name)>1000) copy($path.$name, $login_img);
+			        }
+                }
+            }
+            break;
+        }
+    }
 }
 ?>
